d7058e6c0b3b9569d72a44cc6b8f59fad8876d8f
Khoi1911 <phamtranxuankhoi@gmail.com>
2023-06-05 21:02:19 +0700

    khoi update 5/6/2023

---
+++ tictactoe/src/tictactoe/Tictactoe.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tictactoe;
+import com.formdev.flatlaf.intellijthemes.*;
+/**
+ *
+ * @author Admin
+ */
+public class Tictactoe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // TODO code application logic here
+        //nộp bài: tạo thư mục: <tên nhóm>_<mssv các thành viên>
+        //gồm 4 mục con doc(chứa file báo cáo, file hướng dẫn cài đặt và sử dụng), src(copy project vào), data(file sql), program(phần mềm đi kèm ngoài netbean và xampp)
+        //zip lại nộp trên moddle
+        //file báo cáo: timenewroman-13, cách dòng 1.5, tab/2
+        // cách đề mục: chương 1
+        //1.
+        //1.1
+        //1.2(tối đa là x.x.x)
+        FlatArcDarkOrangeIJTheme.setup();
+        //new MainDisplay();
+        new X3();
+    }
+    
+}
